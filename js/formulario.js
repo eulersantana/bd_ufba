fde7d5af31af4b0e513c8e34d5a11224373057c5
amandasmg <soterodemenezes@gmail.com>
2014-11-02 19:34:43 -0300

    copiando para o diretorio view principal

--- js/formulario.js
+++ js/formulario.js
@@ -2,60 +2,72 @@ $(document).ready(function(){
  var max_fields = 10;
  var botao = $('#add_button');
  var formulario = $('#activ');
- var campo = [" <br/> <div style=' width: 27%; margin: 0px auto;'>",
-" <div class='cronograma' align='center' style='width:300px;height:430px; border: 1px solid black;text-align:center' >           ",
- "        <br/>",
-"          <!-- Text input NOME DA ATIVIDADE-->",
-"          <div class='control-group'>",
-"            <label class='control-label' for='nomeAtividade'>Nome da Atividade</label>",
-"            <div class='controls'>",
-"              <input id='nomeAtividade' name='nomeAtividade' type='text' class='input-xlarge'>",
-"            </div>",
-"          </div>",
-"          <br/>",
-"          <!--Data-->",
-"          <div class='control-group'>",
-"            <label class='control-label' for='dataAtividade'>Data </label>",
-"            <br/>",
-"            <input style='width:150px;height:25px' id='dataAtividade' type='date'>",
-"          </div>",
-"          <br/>",
-"          <!-- Select Basic DEPARAMENTO -->",
-"          <div class='control-group'>",
-"            <label class='control-label' for='departamento'>Departamento</label>",
-"            <div class='controls'>",
-"              <select id='departamento' name='departamento' class='input-xlarge'>",
-"                <option>Option one</option>",
-"                <option>Option two</option>",
-"              </select>",
-"            </div>",
-"          </div>",
-"          <br/>",
-"          <!-- Select Basic INSTALACAO-->",
-"          <div class='control-group'>",
-"            <label class='control-label' for='instalacao'>Instalacao</label>",
-"            <div class='controls'>",
-"              <select id='instalacao' name='instalacao' class='input-xlarge'>",
-"                <option>Option one</option>",
-"                <option>Option two</option>",
-"              </select>",
-"            </div>",
-"          </div>",
-"          <br/>",
-"          <!-- Select Basic CAMPUS-->",
-"          <div class='control-group'>",
-"            <label class='control-label' for='campus'>Campus</label>",
-"            <div class='controls'>",
-"              <select id='campus' name='campus' class='input-xlarge'>",
-"                <option>Option one</option>",
-"                <option>Option two</option>",
-"              </select>",
-"            </div>",
-"          </div>",
-"<br/>",
-"	<input type='button' id='remove_button' value='Remover Atividade'/>",
-"</div>",
-"</div>"].join('\n'); 
+ var campo = ["<p></p>","<legend></legend>", "<ul>",
+                        "<fieldset>",
+							"<div class='cronograma'align='left'> ",
+                                "<div class='row control-group'>",
+                                    "<div class='form-group col-xs-12 floating-label-form-group controls'>",
+                                        "<label>Nome da Atividade</label>",
+                                        "<input id='nomeAtividade' type='text' class='form-control' placeholder='Nome da Atividade' required data-validation-required-message='Por favor, digite um nome para sua atividade.'>",
+                                    "</div>",
+                                "</div>",
+
+                                "<div class='row control-group'>",
+                                    "<div class='form-group col-xs-4 floating-label-form-group controls'>",
+                                        "<label>Data da Atividade</label>",
+                                        "<input type='date' class='form-control' id='dataAtividade' required data-validation-required-message='Por favor, digite a data da Atividade.'>",
+                                    "</div>",
+                                "</div>",
+									"<div class='row control-group'>",
+                                    "<div class='form-group col-xs-4 floating-label-form-group controls'>",
+                                        "<label>Hora da Atividade</label>",
+                                        "<input id='horaAtividade' type='time'>",
+                                    "</div>",
+                                "</div>",
+									"<div class='row control-group'>",
+                                    "<div class='form-group col-xs-12 floating-label-form-group controls'>",
+                                        "<label>Descrição do Evento</label>",
+                                        "<textarea rows='3' cols='50' placeholder='Descreva a Atividade do seu Evento' id='descricaoAtividade' name='descricaoAtividade'></textarea>",
+                                    "</div>",
+                                "</div>",
+									"<div align='left'>",
+										"<div class='row control-group'>",
+                                        "<label  for='btnDepartamento'>Departamento",
+                                        "</label>",
+                                        "<div class='controls'>",
+                                            "<select>",
+                                                "<option value='volvo'>Volvo</option>",
+                                            "</select>",
+                                        "</div>",
+                                    "</div>",
+										"<br/>",
+										"<div class='row control-group'>",
+                                        "<label  for='btnInstalacao'>Instalação",
+                                        "</label>",
+                                        "<div class='controls'>",
+                                            "<select>",
+                                                "<option value='volvo'>Volvo</option>",
+                                            "</select>",
+                                        "</div>",
+                                    "</div>",
+									"<br/>",
+									"<div class='row control-group'>",
+                                        "<label  for='btnCampus'>Campus",
+										"</label>",
+                                        "<div class='controls'>",
+                                         "<select>",
+                                           "<option value='volvo'>Volvo</option>",
+                                       "</select>",
+                                   "</div>      ",
+                                   "<p></p>",
+						                  "<p><input type='button' id='remove_button' value='Remover Atividade'/></p>",
+                               "</div>",
+                           "</div>",
+                       "</div>",
+                       "<!--fim div cronograma-->",
+                     "</fieldset>",
+                        "</ul>",				
+			'<p></p>'].join('\n'); 
   
  var x =0;
 
@@ -64,15 +76,29 @@ $(document).ready(function(){
       x++;
       $(formulario).append(campo);
 	  $('.cronograma').last().hide();
-	  $('.cronograma').last().slideDown('slow');//
-	  $('.cronograma').last().fadeIn('slow'); 
+	  $('.cronograma').last().slideDown("slow");//
+	  $('.cronograma').last().fadeIn("slow"); 
    }
  });
 
  $(formulario).on('click','#remove_button', function(){
-	$(this).parent('div').slideUp('slow');
-	$(this).parent('div').fadeOut('slow',function(){$(this).parent('div').remove();}); 
+	$(this).parents('.cronograma').slideUp("slow");
+	$(this).parents('.cronograma').fadeOut("slow",function(){$(this).parents('.cronograma').remove();}); 
     x--;
+ });
+ 
+ $("#enviar_btn").click(function(){
+	$("#enviar").click(); 
+ });	
+
+$("#entrar_btn").click(function(){
+//IMPLEMENTAR AUTENTICAÇÃO 
+	$(location).attr('href', 'indexUserLogado.html');
+ });
+ 
+ $("#sair_btn").click(function(){
+//IMPLEMENTAR AUTENTICAÇÃO 
+	$(location).attr('href', 'index.html');
  });
  
 });
