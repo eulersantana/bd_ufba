5aa90c30a638f8a45a8b4c183cfde74758d26f2f
dennsilessa <dennsilessa@genesis>
2014-11-04 02:45:35 -0300

    ...

--- views/index.php
+++ views/index.php
@@ -53,7 +53,25 @@
         <script src="js/cadastroAcademico.js"></script>
         <!--<link rel="stylesheet" type="text/css" href="cadastroAcademico.css">-->
 
+		<script type="text/javascript">
+			function add_atividade() {
+				if(confirm("Você irá adicionar uma nova atividade. Continuar?")) {
+					var form_atividades  = document.getElementById("form_atividades");
+					var novas_atividades = document.getElementById("novas_atividades");
+	
+					novas_atividades.innerHTML += form_atividades.innerHTML;
+				}
+			}
 
+			function add_apoiador() {
+				if(confirm("Você irá adicionar um novo apoiador. Continuar?")) {
+					var form_apoiadores  = document.getElementById("form_apoiadores");
+					var novos_apoiadores = document.getElementById("novos_apoiadores");
+
+					novos_apoiadores.innerHTML += form_apoiadores.innerHTML;
+				}
+			}
+		</script>
     </head>
 
     <body id="page-top" class="index">
